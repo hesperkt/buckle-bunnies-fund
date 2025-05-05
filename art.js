91b18135d5a024e3fc04727cb8cbf98c25d7eec8
hesperkt <hk3@wellesley.edu>
2025-05-04 21:58:58 -0400

    updated js folder

--- art.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const zineLinks = document.querySelectorAll('.zine-link');
-  
-    zineLinks.forEach(link => {
-      link.addEventListener('click', (e) => {
-        alert(`You are about to open: ${e.target.innerText}`);
-      });
-    });
-  });
